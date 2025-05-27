56b5fdc7700c04cec3045ddbb3b04684a106d769
alkns0 <alkonsoxdf@gmail.com>
2025-05-27 13:53:58 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,39 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
+import java.util.Stack;
 public class Main
 {
+    
+    public class Paciente {
+        String nombre;
+        String apellido;
+        String id;
+        int categoria;
+        long tiempoLlegada;
+        String estado;
+        String area;
+        Stack <String> historialCambios;
+        
+        public Paciente(String nombre, String apellido, String id){
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.id = id;
+        }
+        
+        public long tiempoEsperaActual(){
+            return 0;
+        }
+        
+        void registrarCambio(String descripcion){
+            
+        }
+        
+        String obtenerUltimoCambio(){
+            return "";
+        }
+        
+        
+    }
+    
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
 }
-
-
-	
